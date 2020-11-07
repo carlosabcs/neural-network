b27fffc7b42737a17cd35371f61919cb4b461fb1
Claudia Suarez Mariscal <131094@unsaac.edu.pe>
2020-11-07 12:36:11 -0300

    fix back-propagation

--- neural_network.py
+++ neural_network.py
@@ -31,12 +31,10 @@ class NeuralNetwork:
 
     def __calculate_error(self, predicted, target):
         # changed by Claudia
-        # please check if the change is correct
         first_mult = np.multiply((-1 * target), np.log(predicted))
         second_mult = np.multiply((1 - target), np.log(1 - predicted))
-        n = len(target)
 
-        return np.sum(first_mult - second_mult)/n
+        return np.sum(first_mult - second_mult)
 
 
     def test_backpropagation(self):
@@ -44,7 +42,7 @@ class NeuralNetwork:
         print(
             'Inicializando rede com a seguinte estrutura de neurônios por camadas: %s\n' %
             ([ self.input_layer_size ] + self.hidden_layers_sizes + [ self.output_layer_size ])
-        )
+            )
 
         for i, layer_weights in enumerate(self.weights):
             print('Theta%s inicial (pesos de cada neuronio, incluindo bias, armazenados nas linhas):' % (i + 1))
@@ -55,12 +53,13 @@ class NeuralNetwork:
         print('Conjunto de treinamento:')
         for i, item in enumerate(self.data):
             print((' ' * 4 ) + 'Exemplo ', i + 1)
-            print((' ' * 8) + 'x: ', item)
+            print((' ' * 8 ) + 'x: ', item)
             print((' ' * 8 ) + 'y: ', self.outputs[i])
 
         print('-------------------------------------------------------------')
         print('Calculando erro/custo J da rede')
         J = 0
+        activations_by_layers = []
         activations = [[] for i in range(self.n_layers)]
         for i in range(len(self.data)):
             print(' ' * 4 + 'Procesando exemplo de treinamento', i + 1)
@@ -72,29 +71,31 @@ class NeuralNetwork:
             print('%sa%s: %s' % (' ' * 8, 1, data[0]))
             print('')
             # Run layer by layer
-            for k in range(1, self.n_layers - 1):
+            for k in range(1, self.n_layers):
                 z = np.dot(self.weights[k - 1], data[k - 1])
                 a = 1 / (1 + np.exp(-1 * z))
                 activations[k] = np.concatenate(([1], a), axis=0)
-                data.append(activations[k])
+                from_index = 0
+                if k == self.n_layers - 1:
+                    # For the last layer
+                    self.predictions.append(a)
+                    from_index = 1
+                else:
+                    data.append(activations[k])
                 print('%sz%s: %s' % (' ' * 8, k + 1, z))
-                print('%sa%s: %s' % (' ' * 8, k + 1, data[k]))
+                print('%sa%s: %s' % (' ' * 8, k + 1, activations[k][from_index:]))
                 print('')
-            # Run last layer
-            last_layer_idx = len(self.hidden_layers_sizes)
-            z = np.dot(self.weights[last_layer_idx], data[last_layer_idx])
-            a = 1 / (1 + np.exp(-1 * z))
-            activations[self.n_layers - 1] = a
-            self.predictions.append(a)
-            print('%sz%s: %s' % (' ' * 8, last_layer_idx + 2, z))
-            print('%sa%s: %s' % (' ' * 8, last_layer_idx + 2, a))
-            print('\n%sf(x): %s' % (' ' * 8, a))
-            print(' ' * 4 + 'Saída predita para o exemplo %s: %s' % (i+1, a))
+
+            # Show prediction
+            predicted = self.predictions[-1]
+            print('\n%sf(x): %s' % (' ' * 8, predicted))
+            print(' ' * 4 + 'Saída predita para o exemplo %s: %s' % (i+1, predicted))
             print(' ' * 4 + 'Saída esperada para o exemplo %s: %s' % (i+1, self.outputs[i]))
             # Calculate error
-            error = self.__calculate_error(a, self.outputs[i])
+            error = self.__calculate_error(predicted, self.outputs[i])
             print(' ' * 4 + 'J do exemplo %s: %.3f\n' % (i+1, error))
             J += error
+            activations_by_layers.append(activations)
 
         # Total error with regularization
         J = J / len(self.data)
@@ -114,9 +115,9 @@ class NeuralNetwork:
                 deltas[k - 1] = np.multiply(
                     np.multiply(
                         np.dot(np.transpose(self.weights[k - 1]), deltas[k]),
-                        activations[k - 1]
+                        activations_by_layers[i][k - 1]
                     ),
-                    ( 1 - activations[k - 1] )
+                    ( 1 - activations_by_layers[i][k - 1] )
                 )
                 deltas[k - 1] = deltas[k - 1][1:]
                 print(' ' * 8 + 'delta%s: %s' % (k, deltas[k - 1]))
